e094b76bcd9d2857115408941e36e03cfca930ab
Nitin Agarwal <nitinag@bluestacks.com>
2021-04-20 01:00:04 +0530

    adding code with machine learning pipeline model will be created, evaluated and finally pickled

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,75 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, make_scorer
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+import warnings
+import re
+import pickle
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+import nltk
+nltk.download(['punkt', 'wordnet', 'stopwords'])
+from sklearn.metrics import classification_report
+
 
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('MessageCategories2', engine)
+    X = df['message']
+    y = df.iloc[:,4:]
+    category_names = y.columns
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
+    text = re.sub(r'[^a-zA-Z0-9]',' ',text.lower())
+    
+    #token messages
+    words = word_tokenize(text)
+    tokens = [w for w in words if w not in stopwords.words("english")]
+    
+    #sterm and lemmatizer
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))])
+    
+    parameters = {
+        'clf__estimator__n_estimators': [50, 100]
+        } 
+    cv = GridSearchCV(pipeline, param_grid=parameters, cv=2, verbose=3)    
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    print(classification_report(y_pred, Y_test.values, target_names=category_names))
+    # print raw accuracy score 
+    print('Accuracy Score: {}'.format(np.mean(Y_test.values == y_pred)))
 
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
